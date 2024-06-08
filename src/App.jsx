b7f71461522adfc69fc21556ee2af446b3fdb76a
Matheo <mflorez3@live.com>
2024-06-08 02:14:38 -0400

    successfully added name randomizer animtion by cycling through array of names using set interval/timeout

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,32 @@ function App() {
 
   const handleSaveNames = (e) => {
     e.preventDefault();
-    if (textAreaVal !== '') {
+    if (textAreaVal !== '' || textAreaVal !== ' ') {
       const splitArray = textAreaVal.split(',').map((name) => name.trim());
       setNameList(splitArray);
     }
   };
 
   const clearLocalNames = () => {
+    setTextAreaVal('');
     return localStorage.removeItem('names');
   };
 
   const handleRandomName = () => {
-    const randomIndex = Math.floor(Math.random() * nameList.length);
-    setSelectedName(nameList[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * nameList.length);
+    // setSelectedName(nameList[randomIndex]);
+    const interval = setInterval(
+      () =>
+        setSelectedName(nameList[Math.floor(Math.random() * nameList.length)]),
+      60
+    );
+    // return () => clearInterval(interval);
 
-    // setTimeout(() => {
-    //   const shuffle = nameList.sort(() => Math.random() - 0.5);
+    const timer = setTimeout(() => {
+      clearInterval(interval);
+    }, 3500);
 
-    //   return setSelectedName(shuffle);
-    // }, 3000);
+    return () => clearTimeout(timer);
   };
 
   return (
@@ -58,7 +65,10 @@ function App() {
               className="bg-neutral-100 w-80 outline-double outline-green-800 outline-2 rounded-sm shadow-md"
             ></textarea>
             <div className="flex justify-evenly">
-              <button className="px-2 py-1 rounded-md bg-[#008a00] hover:bg-[#007c16] text-white text-xl hover:scale-105 shadow-md  mt-4 active:scale-95 transition transform duration-200 ease-in-out">
+              <button
+                type="submit"
+                className="px-2 py-1 rounded-md bg-[#008a00] hover:bg-[#007c16] text-white text-xl hover:scale-105 shadow-md  mt-4 active:scale-95 transition transform duration-200 ease-in-out"
+              >
                 Save Names
               </button>
               <button
